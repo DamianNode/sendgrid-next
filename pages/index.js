8c65da5328fae29828ba97f72bc361659c6c8118
Damian Janus <damian.janus@gmail.com>
2021-12-24 15:56:07 +0100

    Add some styles, try api routes

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,19 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    const formData = {}
+    Array.from(e.currentTarget.elements).forEach(field => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    fetch('/api/mail', {
+      method: 'post',
+      body: JSON.stringify(formData)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,42 +26,27 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          SendGrid Email form
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+          <form method='post' onSubmit={handleOnSubmit}>
+            <p className={styles.row}>
+              <label htmlFor='name'>Name:</label>
+              <input type="text" name="name" />
             </p>
-          </a>
+            <p className={styles.row}>
+              <label htmlFor='email'>Email:</label>
+              <input type="email" name="email" />
+            </p>
+            <p className={styles.row}>
+              <label htmlFor='message'>Message:</label>
+              <textarea name="message" rows={4} />
+            </p>
+            <p className={styles.rowButtons}>
+              <button type='submit'>Send 📨</button>
+            </p>
+          </form>
         </div>
       </main>
 
